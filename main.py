3936935783acaabafd0a60348119e682fb29e2a7
VictorRodas99 <victormatiasrodas100@gmail.com>
2021-07-24 21:52:41 -0400

    update

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 import scrapper as scpr
 
 
@@ -7,10 +7,23 @@ app = Flask(__name__)
 
 @app.route('/', methods=["POST", "GET"])
 def render():
-    
-    return render_template("home.html")
+    if request.method == "POST":
+        busqueda = request.form['busqueda']
+        busqueda = busqueda.replace(" ", "+")
+
+        url_base_search = "https://www.casanissei.com/py/catalogsearch/result/?q="+busqueda
+        url_pibot_search = "https://www.casanissei.com"
+
+        datos, nombre_producto, img_link = scpr.data(url_base_search, url_pibot_search)
+        return render_template("home.html", datos=datos, nombre=nombre_producto, img_link=img_link)
+    else:
+        url_base = "https://www.casanissei.com/py/informatica"
+        url_pibot = "https://www.casanissei.com/py/"
+
+        datos, nombre_producto, img_link = scpr.data(url_base, url_pibot)
+        return render_template("home.html", datos=datos, nombre=nombre_producto, img_link=img_link)
 
 
 
-if __name__=="__main__":
+if __name__=="__main__":    
     app.run(debug=True)
--- scrapper.py
+++ scrapper.py
@@ -2,15 +2,19 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
-
-def data():
-    page = requests.get("https://www.casanissei.com/py/informatica")
+def data(url_base, pibot):
+    page = requests.get(url_base)
     soup = BeautifulSoup(page.content, "html.parser")
     main = soup.find_all("main", class_="page-main")
 
     container = main[0].find_all("div", class_="main-container")
     grid = container[0].find_all("div", class_="products-grid")
-    li = grid[0].find_all("li", class_="product-item")
+    try:
+        li = grid[0].find_all("li", class_="product-item")
+    except IndexError:
+        nulo = "None"
+        print("Sin resultados")
+        return nulo
 
     longitud = len(li)
     list_link = []
@@ -19,47 +23,70 @@ def data():
         link_product = data.find_all("a",  class_="product-item-link")
         list_link.append(link_product[0].attrs['href'])
 
-    url_base = "https://www.casanissei.com/py/"
+    url_base = pibot
+    nombre_producto = []
     datos = []
     list_img_link = []
+    contador = 0
     for link in list_link:
+        contador = contador +1
+        print(contador)
         detalles = {}
         url_final = url_base+link
         page = requests.get(url_final)
         soup = BeautifulSoup(page.content, "html.parser")
         main = soup.find_all("main", class_="page-main")
 
+        name = main[0].find("h1", class_="page-title").text
+
         product = main[0].find_all("div", class_="product media")
         info_product = main[0].find_all("div", class_="product-info-price")
-        detalles_product = info_product[0].find("ul")
-        li = detalles_product.find_all("li")
 
-        c = 0
-        for i in li:
-            key = li[c].strong.text
-            c = c+1
-            value = i.text.replace(key, "")
-            detalles[key]=value
-        
-        for keys in detalles: #por alguna razón las values tenían "\xa0" como primer elemento... entonces lo quitamos aquí
-                values = detalles[keys]
-                detalles[keys] = values[1:]
+        try:
+            detalles_product = info_product[0].find("ul")
+            li = detalles_product.find_all("li")    
 
-        precio = info_product[0].find("span", class_="price").text
-        detalles['Precio'] = precio
+            c = 0
+            for i in li:
+                key = li[c].strong.text
+                c = c+1
+                value = i.text.replace(key, "")
+                detalles[key]=value
+            
 
-        # print(detalles)
-        # print("------------------------------------")
+            script = product[0].find("script", type="text/x-magento-init").string
+            dic_org = json.loads(script)
+            dic_2 = dic_org['[data-gallery-role=gallery-placeholder]']
+            dic_3 = dic_2['mage/gallery/gallery']
+            dic = dic_3['data']
+            dic = dic[0]
+            img_src = dic['img']
+            list_img_link.append(img_src)
+            
+            for keys in detalles: #por alguna razón las values tenían "\xa0" como primer elemento... entonces lo quitamos aquí
+                    values = detalles[keys]
+                    detalles[keys] = values[1:]
 
-        datos.append(detalles)
+            precio = info_product[0].find("span", class_="price").text
+            detalles['Precio'] = precio
 
-        script = product[0].find("script", type="text/x-magento-init").string
-        dic_org = json.loads(script)
-        dic_2 = dic_org['[data-gallery-role=gallery-placeholder]']
-        dic_3 = dic_2['mage/gallery/gallery']
-        dic = dic_3['data']
-        dic = dic[0]
-        img_src = dic['img']
-        list_img_link.append(img_src)
+            nombre_producto.append(name)
+            datos.append(detalles)
+        except AttributeError:
+            continue
+    
+    return datos, nombre_producto, list_img_link
 
-    return datos, list_img_link
+#---------------------Para página principal-------------------------
+url_base = "https://www.casanissei.com/py/informatica"
+url_pibot = "https://www.casanissei.com/py/" #Para el for
+#--------------------------------------------------------------------
+
+#----------------------Para búsqueda--------------------------------
+busqueda = "Valor sacado del input del html" #input("Ingrese lo que quiere buscar: ")
+busqueda = busqueda.replace(" ", "+")
+url_base_search = "https://www.casanissei.com/py/catalogsearch/result/?q="+busqueda
+url_pibot_search = "https://www.casanissei.com"
+#-------------------------------------------------------------------
+
+#data(url_base_search, url_pibot_search)
