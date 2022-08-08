a38764f6944503a237332ca43098eb695186583d
Victor Rodas <victormatiasrodas100@gmail.com>
2022-08-07 20:44:01 -0400

    improve scrapper performance

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import render_template, request
+from scrapper import data
 import json
-import scrapper as scpr
 
 
 app = Flask(__name__)
@@ -12,25 +12,23 @@ def render():
         busqueda = request.form['busqueda']
         busqueda = busqueda.replace(" ", "+")
 
-        url_base_search = "https://www.casanissei.com/py/catalogsearch/result/?q="+busqueda
-        url_pibot_search = "https://www.casanissei.com"
+        url = "https://www.casanissei.com/py/catalogsearch/result/?q="+busqueda
 
         with open("./data.json", "r") as f:
             carrito = json.load(f)
         
         print("_-_-_-_-_-Enviados: ",carrito)
 
-        datos, nombre_producto, img_link = scpr.data(url_base_search, url_pibot_search)
+        datos, nombre_producto, img_link = data(url)
         return render_template("home.html", carrito=carrito, datos=datos, nombre=nombre_producto, img_link=img_link)
     else:
         with open("./data.json", "w") as f:
             f.write("[]")
         
-        url_base = "https://www.casanissei.com/py/informatica"
-        url_pibot = "https://www.casanissei.com/py/"
+        url = "https://www.casanissei.com/py/informatica"
 
         carrito = []
-        datos, nombre_producto, img_link = scpr.data(url_base, url_pibot)
+        datos, nombre_producto, img_link = data(url)
         
         return render_template("home.html", carrito=carrito, datos=datos, nombre=nombre_producto, img_link=img_link)
 
