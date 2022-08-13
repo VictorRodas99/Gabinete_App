f251bcbeea826e70019d1044a3da4919d79aeb83
Victor Rodas <victormatiasrodas100@gmail.com>
2022-08-13 15:24:47 -0400

    improve server performance and scraper changed

--- main.py
+++ main.py
@@ -1,51 +1,38 @@
-from flask import Flask
-from flask import render_template, request
-from scrapper import data
-import json
+from scraper.core.spiders.scraper import ScraperSpider
+from flask import Flask, render_template, request
+from scrapy.signalmanager import dispatcher
+from scrapy.crawler import CrawlerRunner
+from flask_socketio import SocketIO
+from scrapy import signals
+import crochet
 
+crochet.setup()
 
 app = Flask(__name__)
+socket = SocketIO(app)
+runner = CrawlerRunner(settings=None)
+base_url = "https://www.casanissei.com/py/informatica"
+#search_url = "https://www.casanissei.com/py/catalogsearch/result/?q="+busqueda
 
-@app.route('/home', methods=["POST", "GET"])
+@app.route('/')
 def render():
-    if request.method == "POST":
-        busqueda = request.form['busqueda']
-        busqueda = busqueda.replace(" ", "+")
-
-        url = "https://www.casanissei.com/py/catalogsearch/result/?q="+busqueda
-
-        with open("./data.json", "r") as f:
-            carrito = json.load(f)
-        
-        print("_-_-_-_-_-Enviados: ",carrito)
-
-        datos, nombre_producto, img_link = data(url)
-        return render_template("home.html", carrito=carrito, datos=datos, nombre=nombre_producto, img_link=img_link)
-    else:
-        with open("./data.json", "w") as f:
-            f.write("[]")
-        
-        url = "https://www.casanissei.com/py/informatica"
-
-        carrito = []
-        datos, nombre_producto, img_link = data(url)
-        
-        return render_template("home.html", carrito=carrito, datos=datos, nombre=nombre_producto, img_link=img_link)
+    scraper(base_url)
+    return render_template('new_home.html')
 
 
-@app.route('/', methods=["POST", "GET"])
-def select():
-    if request.method == "POST":
-        sel = request.json
-        
-        with open('data.json', 'w') as data:
-            json.dump(sel, data)
-
-        print("-------------------------------",sel,"-----------------------------")
-        return render_template('sel.html', sel=sel)
-    else:
-        return render_template('sel.html')      
+@crochet.run_in_reactor
+def scraper(url):
+    dispatcher.connect(get_data, signal=signals.spider_closed)
+    runner.crawl(ScraperSpider, url=url)
 
 
-if __name__=="__main__":    
-    app.run(debug=True)
+socket.on('event')
+def get_data(spider):
+    data = spider.data
+    print(data[0]['name'], end="\n\n") #Debug
+
+    socket.emit('Response', data)
+
+
+if __name__ == '__main__':
+    socket.run(app, host='0.0.0.0', debug=True)
